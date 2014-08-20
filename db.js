89aa444210232414cb91546acf8c9b4e2445e9d2
Aliaksandr Rahalevich <saksmlz@gmail.com>
2014-08-21 04:32:25 +0800

    setup heroku

--- db.js
+++ db.js
@@ -7,4 +7,18 @@ var Server = new Schema({
 });
 
 mongoose.model('Server', Server);
-mongoose.connect('mongodb://localhost/staging_manager');
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/staging_manager';
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
