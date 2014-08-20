f97ed70480231c395ff9aa32abbba6f6fe2579dc
Aliaksandr Rahalevich <saksmlz@gmail.com>
2014-08-21 04:13:03 +0800

    re #1 - list servers from DB

--- seed.js
+++ seed.js
@@ -3,8 +3,8 @@
 db.dropDatabase();
 
 [
-  { name: 'staging 1', zxc: 1, ip_address: '50.116.11.135' },
-  { name: 'staging 2', zxc: 1, ip_address: '173.255.247.185' },
-  { name: 'staging 3', zxc: 1, ip_address: '23.92.24.145' },
+  { name: 'staging 1', locked: false, ip_address: '50.116.11.135' },
+  { name: 'staging 2', locked: false, ip_address: '173.255.247.185' },
+  { name: 'staging 3', locked: false, ip_address: '23.92.24.145' },
 ].forEach(function(server) { db.servers.save(server) })
 
